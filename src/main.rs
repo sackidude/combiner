4aafd56c854e9e94eaeb10fc4ee475581c5673a0
sackidude <nils.nilsen9@outlook.com>
2024-04-06 14:20:13 +0200

    args with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    first_path: std::path::PathBuf,
+    second_path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!(
+        "First file: {}\nSecond file: {}",
+        args.first_path.display(),
+        args.second_path.display()
+    )
 }
